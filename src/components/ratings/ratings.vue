3eb070a94201a78d0771bf3922ae3c4c3a043f14
linjunch <1422679209@qq.com>
2019-03-09 06:49:48 +0800

    vue app

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="ratings">
+      我是评论
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ratings"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="seller">
+      我是商家
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "seller"
+    }
+</script>
+
+<style scoped>
+
+</style>
